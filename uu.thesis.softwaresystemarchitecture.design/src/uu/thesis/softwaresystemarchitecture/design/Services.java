ac2e660cf9cdd3b7e83c7eb90290ca13a3eb8770
Jan van Rhijn <j.vanrhijn@students.uu.nl>
2019-07-08 11:15:33 +0200

    Additional feature to add viewpoints in the editor

--- uu.thesis.softwaresystemarchitecture.design/src/uu/thesis/softwaresystemarchitecture/design/Services.java
+++ uu.thesis.softwaresystemarchitecture.design/src/uu/thesis/softwaresystemarchitecture/design/Services.java
@@ -1,8 +1,5 @@
 package uu.thesis.softwaresystemarchitecture.design;
 
-//import java.util.ArrayList;
-//import java.util.List;
-
 import uu.thesis.emf.metamodel.softwaresystemarchitecture.SoftwareSystemArchitecture.*;
 
 /**
@@ -13,7 +10,7 @@ public class Services {
     * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
     */
     
-	//TODO dit kan wellicht mooier
+	
 	public Boolean InformationflowRed(Functional_Architecture_Model fam, Informationflow informationflow) {
 		for(Scenario scenario:fam.getScenario_overlay()) {
 	    	for(Interaction interaction:scenario.getInteraction()) {
@@ -69,33 +66,4 @@ public class Services {
 		return false;
 	}
 
-//	public void NotReferencedFeatures(Software_System softwareSystem) {
-//		Deployment_Model deploymentModel = null;
-//		Functional_Architecture_Model fam = null;
-//		
-//		List<Feature> allFeatures = new ArrayList<Feature>();
-//		List<Feature> referencedFeatures = new ArrayList<Feature>();
-//		
-//		for(Viewpoint viewPoint:softwareSystem.getViewpoint()) {
-//			if(viewPoint.eClass().getName() == "Deployment_Model") {
-//				deploymentModel = (Deployment_Model) viewPoint;
-//			} else if (viewPoint.eClass().getName() == "Functional_Architecture_Model") {
-//				fam = (Functional_Architecture_Model) viewPoint;
-//			}
-//		}
-//		
-//		for(Component component:deploymentModel.getContains()) {
-//			for(Feature feature:component.getContains()) {
-//				referencedFeatures.add(feature);
-//			}
-//		}
-//		
-//		for(Scenario scenario:fam.getScenario_overlay()) {
-//			for(Interaction interaction :scenario.getInteraction()) {
-//				allFeatures.add(interaction.getInformationflow().getReceiver());
-//				allFeatures.add((Feature) interaction.getInformationflow().eContainer());
-//			}
-//		}
-//		
-//	}
 }
